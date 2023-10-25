62f8a473729c5d274fc04dfdaa763ba1185d7ce6
uMelo21 <gustavomelo2805@gmail.com>
2023-10-25 16:12:40 -0300

    Create form.php

---
+++ index/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+<div class="form">
+    <form action="/cadprod.php"> 
+        <div class="title"><h2>Adicionar um novo produto</h2></div> <br>
+       Produto <input name="produto" type="text">
+       Preço <input name="preco" type="text">
+       Imagem <input name="img" type="text">
+       Descrição <input name="descricao" type="text">
+       <button type="submit"> Adicionar </button>
+    </form>
+</div>
+</body>
+</html>
