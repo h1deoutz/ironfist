0f0297b5fd1212c55e76a1a067067a66dadf53a9
uMelo21 <gustavomelo2805@gmail.com>
2023-10-25 17:44:28 -0300

    Update compuser.php

--- index/compuser.php
+++ index/compuser.php
@@ -3,15 +3,19 @@
 $nome_completo = $_GET ['nome_completo'];
 $CPF = $_GET ['CPF'];
 $data_nascimento = $_GET ['data_nascimento'];
-$CEP_CNPJ = $_GET ['CEP_CNPJ']
-$endereco = $_GET ['endereco']
-$cidade = $_GET ['cidade']
-$UF = $_GET ['UF']
-$genero = $_GET ['genero']
+$CEP_CNPJ = $_GET ['CEP_CNPJ'];
+$endereco = $_GET ['endereco'];
+$cidade = $_GET ['cidade'];
+$UF = $_GET ['UF'];
+$genero = $_GET ['genero'];
+$id = $_GET ['id'];
 
     include 'connect.php';
-        $sql = "INSERT INTO cad_user (nome_completo, CPF, data_nascimento, CEP_CNPJ, endereco, cidade, UF, genero)
-        VALUES ('$nome_completo', '$CPF', '$data_nascimento', '$CEP_CNPJ', '$endereco', '$cidade', '$UF', '$genero')";
+       // $sql = "INSERT INTO cad_user (nome_completo, CPF, data_nascimento, CEP_CNPJ, endereco, cidade, UF, genero)
+       //VALUES ('$nome_completo', '$CPF', '$data_nascimento', '$CEP_CNPJ', '$endereco', '$cidade', '$UF', '$genero', '$id')";
+       $sql = "UPDATE cad_user
+        SET nome_completo = $nome_completo, CPF = $CPF, data_nascimento, CEP_CNPJ = $CEP_CNPJ, endereco = $endereco, cidade = $cidade, UF = $UF, genero = $genero
+        WHERE id = $id";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
   }     else {
