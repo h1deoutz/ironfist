1ae2070554c58ea4d925131912d4d2a0dfa7eae8
uMelo21 <gustavomelo2805@gmail.com>
2023-10-25 16:12:56 -0300

    Create ajaxExample.php

---
+++ index/ajaxExample.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <title>Exemplo Ajax</title> 
+    <script>
+    function solicitacao() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            document.getElementById("demo").innerHTML = this.responseText;
+            }
+        xhttp.open("GET", "consuser.php", true);
+        xhttp.send();
+        }
+    </script>
+    </head>
+    <body>
+        <button type="button" onclick="solicitacao()" >puxa</button>
+        <div id="demo"></div>
+    </body>
+</html>
