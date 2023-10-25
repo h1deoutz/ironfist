83b6c135d2c3eadf6f7fa41dc74cac67f4b63763
uMelo21 <gustavomelo2805@gmail.com>
2023-10-25 16:47:28 -0300

    Update consuser.php

--- index/consuser.php
+++ index/consuser.php
@@ -7,7 +7,7 @@
         if ($result->num_rows > 0) {
           // output data of each row
           while($row = $result->fetch_assoc()) {
-          echo $row["id"]. ";" . $row["nome"]. ";" . $row["email"]. ";" . $row["senha"]. ";" . $row["CPF"]. ";" . $row["data_de_nascimento"]. ";" . $row["CEP_CNPJ"]. ";" . $row["endereco"]. ";" . $row["cidade"]. ";" . $row["UF"]. ";" . $row["genero"]."<br>";
+          echo $row["id"]. ";" . $row["nome"]. ";" . $row["email"]. ";" . $row["senha"]. ";" . $row["CPF"]. ";" . $row["data_nascimento"]. ";" . $row["CEP_CNPJ"]. ";" . $row["endereco"]. ";" . $row["cidade"]. ";" . $row["UF"]. ";" . $row["genero"]. ";" . $row["nome_completo"]"<br>";
           }
         }
         ?>
