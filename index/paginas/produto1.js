b3ef4a73cf00d967f5338eadf21ab17502573de0
h1deoutz <luizhbcamacho1@gmail.com>
2023-10-31 18:26:03 -0300

    Create produto1.js

---
+++ index/paginas/produto1.js
@@ -0,0 +1,17 @@
+function changeImage1()
+{
+    var img  =  document.getElementById('image');
+    img.src = '../img-svg/ironfist.whey.chocolate.png'
+}
+function changeImage2()
+{
+    var img  =  document.getElementById('image');
+    img.src = '../img-svg/whey2.jpg'
+}
+function changeImage3()
+{
+    var img  =  document.getElementById('image');
+    img.src = '../img-svg/whey3.jpeg'
+}
+
+ 
