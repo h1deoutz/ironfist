b15ae34e2b307b4cadb9444de6ba8897f9f9f815
uMelo21 <gustavomelo2805@gmail.com>
2023-11-03 10:21:21 -0300

    Update cadprod.php

--- index/cadprod.php
+++ index/cadprod.php
@@ -4,10 +4,11 @@ $preco = $_GET ['preco'];
 $produto = $_GET ['produto'];
 $img = $_GET ['img'];
 $descricao = $_GET ['descricao'];
-
-    include 'connect.php';
-        $sql = "INSERT INTO produto (nome_prod, desc_prod, preco, img)
-        VALUES ('$produto', '$descricao', '$preco', '$img')";
+$categoria = $_GET ['categoria']
+    
+include 'connect.php';
+        $sql = "INSERT INTO produto (nome_prod, desc_prod, preco, img, categoria)
+        VALUES ('$produto', '$descricao', '$preco', '$img', '$categoria')";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
   }     else {
