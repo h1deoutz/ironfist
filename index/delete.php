cf398a06679c97245b76fdd5d20af1c686f1f929
uMelo21 <gustavomelo2805@gmail.com>
2023-11-29 17:53:23 -0300

    Create delete.php

---
+++ index/delete.php
@@ -0,0 +1,11 @@
+<?php
+$id = $_GET['id'];
+include 'connect.php';
+$sql = "DELETE FROM produto WHERE id='$id'";
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+$conn->close();
+?>
