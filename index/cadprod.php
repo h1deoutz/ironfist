585a6359d96bdf0989df834fb890f02266bfa6c7
uMelo21 <gustavomelo2805@gmail.com>
2023-11-29 17:53:01 -0300

    Update cadprod.php

--- index/cadprod.php
+++ index/cadprod.php
@@ -1,18 +1,21 @@
 <?php
 
-$preco = $_GET ['preco'];
 $produto = $_GET ['produto'];
+$preco = $_GET ['preco'];
 $img = $_GET ['img'];
 $descricao = $_GET ['descricao'];
-
+$categoria = $_GET ['categoria'];
+    
     include 'connect.php';
-        $sql = "INSERT INTO produto (nome_prod, desc_prod, preco, img)
-        VALUES ('$produto', '$descricao', '$preco', '$img')";
+        $sql = "INSERT INTO produto (nome_prod, desc_prod, preco, img, categoria)
+        VALUES ('$produto', '$descricao', '$preco', '$img', '$categoria')";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
   }     else {
         echo "Error: " . $sql . "<br>" . $conn->error;
   }
-  
+    
   $conn->close();
+  
+  header("location:/ironfist/index/estoque.php")
 ?>
