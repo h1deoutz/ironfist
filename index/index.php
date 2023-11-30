146bbcfc694fce66597866249c7becbe19e992cf
uMelo21 <gustavomelo2805@gmail.com>
2023-11-29 23:06:43 -0300

    Update index.php

--- index/index.php
+++ index/index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+    ?>
 <!doctype html>
 <html lang="pt-br">
   <head>
@@ -10,10 +13,25 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script src="https://kit.fontawesome.com/5a6fff528f.js" crossorigin="anonymous"></script>
-        
   <link rel="stylesheet" href="style.css">
-<body class="body">
-
+  <body class="body" onload="solicitacao()">
+<p>
+            <?php 
+			//Recuperando o valor da variável global, os erro de login.
+			if(isset($_SESSION['loginErro'])){
+                echo $_SESSION['loginErro'];
+                unset($_SESSION['loginErro']);
+            }?>
+        </p>
+        <p>
+            <?php 
+			//Recuperando o valor da variável global, deslogado com sucesso.
+            if(isset($_SESSION['logindeslogado'])){
+                echo $_SESSION['logindeslogado'];
+                unset($_SESSION['logindeslogado']);
+            }
+            ?>
+        </p>
   <header>
   
     <div class="header"  style=" z-index: 2; position: fixed; width: 100%; background-color: #2b0086; height: 48px;" >
@@ -44,16 +62,16 @@
               </span>
               <div class="form-box login">
               <h2>Entrar</h2>
-              <form action="">
+              <form class="form-signing"method= "POST" action="valida.php">
                 <div class="input-box">
                 <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                <input type="email" required>
+                <input type="email" name="email" required>
                 <label>Email</label>
               </div>
               <div class="input-box">
                 <span class="icon"><ion-icon name="lock-closed"></ion-icon></script>
                   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></span>
-                <input type="password" class="input-senha" required>
+                <input type="password" class="input-senha" name="senha" required>
                 <label>Senha</label>
               </div>
               <div class="remember-forgot">
@@ -150,22 +168,7 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-      <input type="button" onClick="solicitacao()">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
+    
       <div class="banner-frete">
         <img src="img-svg/caixa-ironfist.gif" alt="">
         <div class="text"><h2>Frete grátis para compras acima de R$100,00</h2></div>
@@ -445,5 +448,11 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
         
-  </body>
+  <?php
+    echo "Usuario: ". $_SESSION['usuarioNome']; 
+?>
+<br>
+<a href="sair.php">Sair</a>  
+
+</body>
 </html>
