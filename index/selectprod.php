f119dcf8e646dc05316666a3c72230602760d95f
uMelo21 <gustavomelo2805@gmail.com>
2023-11-29 23:07:12 -0300

    Update selectprod.php

--- index/selectprod.php
+++ index/selectprod.php
@@ -1,13 +1,13 @@
 <?php
           include 'connect.php';
 
-        $sql = "SELECT id, nome_prod, desc_prod, preco, img FROM produto";
+        $sql = "SELECT id, nome_prod, desc_prod, preco, img, categoria FROM produto";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
           // output data of each row
           while($row = $result->fetch_assoc()) {
-          echo $row["id"].";" . $row["nome_prod"]. "; " . $row["desc_prod"]. " ;" .$row["preco"]. "<br>";
+            echo "{\"id\":".$row["id"].",\"name\": \"". $row["nome_prod"]."\",\"price\":" .$row["preco"]."image\": \"".$row["img"]."\",\"category\":\"".$row["categoria"]."\",\"link\": \"paginas/produto1.php\"} </br>";  
         
         }
       } 
